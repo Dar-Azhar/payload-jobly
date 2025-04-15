e05adfb41ab8780cf70129b57e6ac2532b1c0363
Azhar Ali <dev.darazhar@gmail.com>
2025-04-15 16:13:20 +0530

    feat: installed and configured tailwind and shadcn with some ready made components

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,11 +1,13 @@
 import { headers as getHeaders } from 'next/headers.js'
-import Image from 'next/image'
 import { getPayload } from 'payload'
 import React from 'react'
-import { fileURLToPath } from 'url'
 
 import config from '@/payload.config'
 import './styles.css'
+import { Menubar } from '@/libs/shadcn/components/ui/menubar'
+import { Button } from '@/libs/shadcn/components/ui/button'
+import { Card } from '@/libs/shadcn/components/ui/card'
+
 
 export default async function HomePage() {
   const headers = await getHeaders()
@@ -13,47 +15,14 @@ export default async function HomePage() {
   const payload = await getPayload({ config: payloadConfig })
   const { user } = await payload.auth({ headers })
 
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
-
   return (
     <div className="home">
-      <div className="content">
-        <picture>
-          <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
-          <Image
-            alt="Payload Logo"
-            height={65}
-            src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
-            width={65}
-          />
-        </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
-        <div className="links">
-          <a
-            className="admin"
-            href={payloadConfig.routes.admin}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Go to admin panel
-          </a>
-          <a
-            className="docs"
-            href="https://payloadcms.com/docs"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Documentation
-          </a>
-        </div>
-      </div>
-      <div className="footer">
-        <p>Update this page by editing</p>
-        <a className="codeLink" href={fileURL}>
-          <code>app/(frontend)/page.tsx</code>
-        </a>
-      </div>
+      <Menubar></Menubar>
+      <h1 className="text-3xl font-bold underline text-center ">
+        Hello world!
+      </h1>
+      <Button className='text-center'>shadCN UI Test</Button>
+      <Card>card</Card>
     </div>
   )
 }
